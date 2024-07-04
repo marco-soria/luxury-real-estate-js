7c5c0d1bb99a02c5653e816235b35eb769072967
marco-soria <marco.soriaa7@gmail.com>
2024-07-04 16:03:22 -0500

    geocoding and added listing item create and edit

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -90,7 +90,7 @@ export default function CreateListing() {
     let location;
     if (geolocationEnabled) {
       const response = await fetch(
-        `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.REACT_APP_GEOCODE_API_KEY}`
+        `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${process.env.VITE_GEOCODE_API_KEY}`
       );
       const data = await response.json();
       console.log(data);
